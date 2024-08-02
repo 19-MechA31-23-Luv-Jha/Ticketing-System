f8d1bbaa81424c79dbcbd64f47a1dfbbb8359f54
Luv Jha <luv7852143jha@gmail.com>
2024-08-02 17:27:26 +0530

    deleted servlet initializer

--- Ticketing System Backend/tickets/src/main/java/com/ticketing/tickets/TicketsApplication.java
+++ Ticketing System Backend/tickets/src/main/java/com/ticketing/tickets/TicketsApplication.java
@@ -2,12 +2,19 @@ package com.ticketing.tickets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TicketsApplication {
+public class TicketsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketsApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TicketsApplication.class);
 	}
 
 }
