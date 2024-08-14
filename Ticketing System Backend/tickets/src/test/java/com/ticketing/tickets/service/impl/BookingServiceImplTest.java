b468968438c2437fc3044a5572b4fcef3514dceb
Luv Jha <luv7852143jha@gmail.com>
2024-08-14 16:15:27 +0530

    changes for test files

---
+++ Ticketing System Backend/tickets/src/test/java/com/ticketing/tickets/service/impl/BookingServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.ticketing.tickets.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceImplTest {
+
+}
--- Ticketing System Backend/tickets/src/test/java/com/ticketing/tickets/testing/BookingServiceImplTest.java
+++
@@ -1,4 +0,0 @@
-package com.ticketing.tickets.testing;
-
-public class BookingServiceImplTest {
-}
